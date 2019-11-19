ce9960c8042d1e897c8221bdc323289fe7150781
visiopo44 <visiopo44@gmail.com>
2019-11-19 10:39:21 +0000

    Eratosthenes's Prime number Completion

--- Eratosthenes.py
+++ Eratosthenes.py
@@ -2,14 +2,17 @@ from math import sqrt
 
 def eratos(N) :
   n=sqrt(N) #routin's max
-  prime=[N]
-  prime.insert(0,False)
-  prime.insert(1,False)
-  for i in range(int(n)) :
+  prime=[True for a in range(int(N)+1)] #Eratosthenes True is prime number
+
+  prime[0] = False
+  prime[1] = False
+
+  for i in range(2,int(n)) :
     if(prime[i] is not False):
-      j=i*i
-      while j<N :
-        prime.insert(j,False)
-        j=j+i
-        
+      for j in range(i,N) :
+        if i*j >= N:
+          break
+        elif i * j<=N:
+          prime[i*j] = False
+
   return prime
--- main.py
+++ main.py
@@ -3,4 +3,7 @@ N = 1214348893105131924078850286591990112509 #p*q (Key)
 e = 65537 # (Key)
 C = 23161252532832885956948212465951275785 #Cipher
 #Secret key d
-print(eratos(N))
+test_N = 20
+p = eratos(test_N)
+for i in range(test_N):
+  print(f"{i}={p[i]}")
